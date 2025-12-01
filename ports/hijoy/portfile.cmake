ece5a6d8cc3ca89c0000748cacdb52f67b34549a
yii <1729907841@qq.com>
2025-12-01 21:20:27 +0800

    1201

--- ports/hijoy/portfile.cmake
+++ ports/hijoy/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO JoyHive/HiJoy
-    REF "${VERSION}"
+    REF a76f6010432789c3c7e0d78c45e66a1cd268cf85
     SHA512 47ff6d289dcc22681eea6da465b0348172921e7cafff8fd57a1540d3232cc6b53250a4625c954ee0944c87963b17680ecbc3ea123e43c2c822efe0dc6fa6cef3
     HEAD_REF main
 )
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/hijoy)
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
