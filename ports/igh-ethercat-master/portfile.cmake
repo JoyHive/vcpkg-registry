4b050ead4067bbe8d36b85ba77822ecbb4661d43
work yii <1729907841@qq.com>
2025-12-03 13:31:59 +0800

    1203

--- ports/igh-ethercat-master/portfile.cmake
+++ ports/igh-ethercat-master/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO JoyHive/igh-ethercat-master
     REF main
-    SHA512 0
+    SHA512 f27591ed1debba3af89047967a22083f341ecd14e72e99fab5940756c71f6126e5e54cdcad6d226bd8a5bb54f613d703615ba7fa49899a04f0a168a5fb8c1bdf
     HEAD_REF main
     AUTHORIZATION_TOKEN "$ENV{GITHUB_TOKEN}"
     USE_TARBALL_API
