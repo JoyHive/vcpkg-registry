a4bedc71361a788160e30c959b246acfe1e5dfbc
yii <1729907841@qq.com>
2026-03-09 21:24:19 +0800

    feat: 移除hijoy的protobuf依赖

--- local-overlay/hijoy/portfile.cmake
+++ local-overlay/hijoy/portfile.cmake
@@ -1,7 +1,5 @@
-set(SOURCE_PATH "/home/yii/work/hijoy")
-
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+    SOURCE_PATH "$ENV{HIJOY_SOURCE_PATH}"
     OPTIONS
         -DHIJOY_INSTALL=ON
         -DHIJOY_OS_TYPE="gnulinux"
@@ -21,4 +19,4 @@ file(REMOVE_RECURSE
 )
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(FILE_LIST "$ENV{HIJOY_SOURCE_PATH}/LICENSE")
